1d36cb09d8c2223659008ccf110a5b90b8ebfff4
RISHAD1997 <rishadnandi98@gmail.com>
2022-09-25 13:07:06 +0530

    about page style changed

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,7 +5,7 @@ import CV from "./Resume.pdf";
 import { motion } from "framer-motion";
 import "./About.css";
 import Footer from "../../Components/Footer/Footer";
-import Slide from "react-reveal/Slide";
+import Pulse from "react-reveal/Pulse";
 
 function About() {
   return (
@@ -26,11 +26,12 @@ function About() {
       <NavigationBar />
       <div className="about">
         <h2 style={{ marginBottom: "5%", fontWeight: "bold" }}>About</h2>
-        <Slide left>
+        <Pulse>
           <p>
-            Every person has their own unique story. Here is a glimpse into
-            mine.
-            <br />
+            <span>
+              Every person has their own unique story. Here is a glimpse into
+              mine.
+            </span>
             <br />
             My name is Rishad P I am currently a Graduate Fellow. I compeleted
             my B-tech in Information Technology at college Of Engineering,
@@ -42,7 +43,7 @@ function About() {
             Now, I’m looking to leverage everything I’ve learned and get some
             hands-on work experience.
           </p>
-        </Slide>
+        </Pulse>
         <a href={CV} download="Resume" className="download-link">
           <Button variant="danger" className="download-button">
             Download CV
